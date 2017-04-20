c25b8a7b5d81487a2fb84e5417665eff6ff2543e
andrewz <andrewz@amayasoftware>
2017-04-20 18:53:32 -0400

    * 如何自动维护数据的一致性（求货物的总金额）   ** 用为什么用 $timeout？ 而不是直接调用？ 也不是发自定义的消息？
    再一次确认： 用的是NG的自动数据绑定
      （不用JQuery， 也不用document.getElementById等等）

--- dataService.js
+++ dataService.js
@@ -2,10 +2,11 @@ angular.module("ShopApp")
     .service('DataService', function ($timeout) {
         // 内部变量
         var self = this,
-            item1 = {name: "海尔电视", price: 2000, quantity: 2};
+            item1 = {name: "海尔电视", price: 2000, quantity: 2, sum:0};
 
         // 对外接口：数据
         self.item1 = item1;
+        self.totalAmount = 0; //这个既是内部使用，也是外部使用，因为他不是地址引用的对象， 只是简单的值变量
 
         // 对外接口：处理函数
         self.add = add;
@@ -14,9 +15,18 @@ angular.module("ShopApp")
         // 内部具体实现
         function add() {
             self.item1.quantity++;
+            $timeout(update);
         }
 
         function subtract() {
             self.item1.quantity--;
+            $timeout(update);
         }
+
+        function update() {
+            item1.sum = item1.price * item1.quantity;
+            self.totalAmount = item1.sum;
+        }
+
+        $timeout(update);
     });
--- demoController.js
+++ demoController.js
@@ -2,6 +2,7 @@ angular.module("ShopApp")
 .controller('DemoController', function (DataService) {
     // 内部变量
     var self = this;
+    self.data = DataService;
     self.item1 = DataService.item1;
     self.add = DataService.add;
     self.subtract = DataService.subtract;
