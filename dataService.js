d28f6bbda6d474fe03f8feb066167df07ebf469c
andrewz <andrewz@amayasoftware>
2017-04-20 18:42:08 -0400

    * 扩充成为最简单的angularjs页面   * 修改页面标题   * 加入ng的库   * 定义项目app的名称，   * 必须是按照 MVC模式的要求， 建立基本框架， 分别定义项目所需要Model， View， Control：   *** 数据模块： DataService.js   *** controller模块: demoController.js   *** 改造DOM模板为NG风格的: index.html

---
+++ dataService.js
@@ -0,0 +1,10 @@
+angular.module("ShopApp")
+    .service('DataService', function ($timeout) {
+        // 内部变量
+
+        // 对外接口：数据
+
+        // 对外接口：处理函数
+
+        // 内部具体实现
+    });
---
+++ demoController.js
@@ -0,0 +1,3 @@
+angular.module("ShopApp")
+.controller('DemoController', function (DataService) {
+});
