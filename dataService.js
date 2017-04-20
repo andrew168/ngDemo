e7de62a993e0a3fce2e9c9ced3ee9c4d89eb4966
andrewz <andrewz@amayasoftware>
2017-04-20 18:48:37 -0400

    * 如何把数据显示到页面上   在页面上显示 商品的名称， 价格和数量： 海尔电视， 2000， 2台，   （数据来自变量 item1 = ｛ name: "海尔电视", price: 2000, quantity: 2}   ** 先在DataService中定义数据，   ** 通过Controller提供给DOM，   ** 在DOM中使用它   用的是NG的自动数据绑定   （不用JQuery， 也不用document.getElementById等等）

--- dataService.js
+++ dataService.js
@@ -1,8 +1,11 @@
 angular.module("ShopApp")
     .service('DataService', function ($timeout) {
         // 内部变量
+        var self = this,
+            item1 = {name: "海尔电视", price: 2000, quantity: 2};
 
         // 对外接口：数据
+        self.item1 = item1;
 
         // 对外接口：处理函数
 
--- demoController.js
+++ demoController.js
@@ -1,3 +1,6 @@
 angular.module("ShopApp")
 .controller('DemoController', function (DataService) {
+    // 内部变量
+    var self = this;
+    self.item1 = DataService.item1;
 });
