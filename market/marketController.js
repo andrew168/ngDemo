d2440471be48e4f39a5ef839cbc5d4b426ab2233
andrew.zhi <andrew.zhi@mobilelive.ca>
2017-05-05 22:16:28 -0400

    增加多个状态3: * 给market状态增加自己的controller， （注意，它只是在大demoController控制区域内的更小的一块div区域）

---
+++ market/marketController.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Andrewz on 5/5/2017.
+ */
+angular.module("ShopApp")
+    .controller('MarketController', function () {
+        // 内部变量
+        var self = this,
+            label = "Welcome to Market!";
+        self.label = label;
+    });
