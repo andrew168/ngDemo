f6cd071b325986edcb5331cf66178a1905642c43
andrewz <andrewz@amayasoftware>
2017-05-16 18:31:20 -0400

    发布项目5： * 把index中的js文件合并成一个大文件， 更新index文件 * minify *并且把结果临时保存到dist目录下

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,45 @@
 'use strict';
-var gulp = require('gulp'),
-    runSequence = require('run-sequence');
-
+var gulp = require('gulp');
+var $ = require('gulp-load-plugins')(); //jshint ignore:line
+var uglifyjs = require("uglify-js");
+var fs = require('fs');
+var del = require('del');
+var args = require('yargs').argv;
+var runSequence = require('run-sequence');
 JSON.minify = JSON.minify || require("node-json-minify");
 
-gulp.task('clean', function () {
-    console.log("start to clean");
-});
+var APP_JS_FILE = "demo-app-all.js",
+    APP_JS_MIN_FILE = "demo-app-all.min.js",
+    APP_JS_MAP = "demo-app-all.map",
+    DIST_FOLDER = "dist/demoApp/";
+
+gulp.task('clean', del.bind(null, ['dist']));
 
 gulp.task('concat-files', function () {
-    console.log("start to concat");
+    var assets = $.useref.assets({searchPath: '{,src},dist'});
+    return gulp.src('index.html')
+        .pipe(assets)
+        .pipe(assets.restore())
+        .pipe($.useref())
+        .pipe(gulp.dest(DIST_FOLDER));
 });
 
 gulp.task('minify', function () {
     console.log("start to minify");
+    var result = uglifyjs.minify([DIST_FOLDER + APP_JS_FILE], {
+        outSourceMap: APP_JS_MAP,
+        sourceRoot: "",
+        compress: {
+            warnings: false
+        }
+    });
+    result.map = result.map.replace('"sources":[' + DIST_FOLDER, '"sources":["');
+    fs.writeFileSync(DIST_FOLDER + APP_JS_MAP, result.map);
+    fs.writeFileSync(DIST_FOLDER + APP_JS_MIN_FILE, result.code);
 });
 
 gulp.task('update-index', function () {
-    console.log("start to update index.html");
+    console.log("start to modify index.html");
 });
 
 gulp.task('deploy', function () {
